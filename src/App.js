f42f2c4dba73758854b3f9265fde05283615c9af
Finn Young <finnyoung88@gmail.com>
2018-10-12 12:33:56 +1300

    Fetch data from auth protected controller.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@ import './App.css';
 
 import AuthService from './components/AuthService';
 import withAuth from './components/withAuth';
-const Auth = new AuthService();
+const Auth = new AuthService('http://localhost:3001');
 
 class App extends Component {
+  fetch_data() {
+    Auth.fetch('/api/v1/hello', {
+      method: 'GET',
+      credentials: 'same-origin'
+    }).then(response => {
+      console.log(response)
+    })
+  }
+
   handleLogout(){
     Auth.logout()
     this.props.history.replace('/login');
@@ -17,6 +26,9 @@ class App extends Component {
         <h2>Welcome {this.props.user.name}</h2>
         <p className="App-intro">
           <button type="button" className="form-submit" style={{width: '100px'}} onClick={this.handleLogout.bind(this)}>Logout</button>
+          <br />
+          <br />
+          <button type="button" className="form-submit" style={{width: '100px'}} onClick={this.fetch_data.bind(this)}>Fetch data</button>
         </p>
       </div>
     );
